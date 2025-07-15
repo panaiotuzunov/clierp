00fbbf4b319e39531992f7b4edc9c85be09a2c81
panaiotuzunov <panaiot.uzunov@gmail.com>
2025-07-15 19:19:03 +0300

    add doc creation and reference

--- entranceReceipt.go
+++ entranceReceipt.go
@@ -1,6 +1,12 @@
 package main
 
-import "time"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
 
 type entranceReceipt struct {
 	id      int
@@ -10,4 +16,43 @@ type entranceReceipt struct {
 	gross   int
 	tare    int
 	net     int
+}
+
+func NewEntranceReceipt(id *int) entranceReceipt {
+	scanner := bufio.NewScanner(os.Stdin)
+	result := entranceReceipt{
+		id: *id,
+	}
+	result.date = time.Now()
+	fmt.Println("Въведете номер на камион.")
+	scanner.Scan()
+	result.truck = scanner.Text()
+	fmt.Println("Въведете номер на ремарке.")
+	scanner.Scan()
+	result.trailer = scanner.Text()
+	fmt.Println("Въведете количество бруто.")
+	for {
+		scanner.Scan()
+		num, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			fmt.Printf("Невалидно число: %v", err)
+			continue
+		}
+		result.gross = num
+		break
+	}
+	fmt.Println("Въведете количество тара.")
+	for {
+		scanner.Scan()
+		num, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			fmt.Printf("Невалидно число: %v", err)
+			continue
+		}
+		result.tare = num
+		break
+	}
+	result.net = result.gross - result.tare
+	*id++
+	return result
 }
--- repl.go
+++ repl.go
@@ -4,25 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"time"
 )
 
 func startRepl() {
 	docEnumerator := 1
-	receipts := []*entranceReceipt{}
+	receipts := []entranceReceipt{}
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Изберете опцията от менюто. (само код)")
-	fmt.Println("1. Справки")
-	fmt.Println("2. Нов документ")
 	for {
+		fmt.Println("Изберете опцията от менюто. (само код)")
+		fmt.Println("1. Справки")
+		fmt.Println("2. Нов документ")
 		scanner.Scan()
 		selection := scanner.Text()
 		switch selection {
 		case "1":
 			reference(receipts)
 		case "2":
-			receipts = append(receipts, newDoc(&docEnumerator))
+			newDoc(&docEnumerator, &receipts)
 		case "exit":
 			os.Exit(0)
 		default:
@@ -31,47 +29,13 @@ func startRepl() {
 	}
 }
 
-func reference(receipts []*entranceReceipt) {
+func reference(receipts []entranceReceipt) {
 	fmt.Printf("%-12v %-12v %-12v %-12v %-12v %-12v %-12v\n", "НОМЕР", "ДАТА", "КАМИОН", "РЕМАРКЕ", "БРУТО", "ТАРА", "НЕТО")
 	for _, e := range receipts {
-		fmt.Printf("%-12v %-12v %-12v %-12v %-12v %-12v %-12v\n", e.id, e.date.Format("dd-mm-yyyy"), e.truck, e.trailer, e.gross, e.tare, e.net)
+		fmt.Printf("%-12v %-12v %-12v %-12v %-12v %-12v %-12v\n", e.id, e.date.Format("00/00/0000"), e.truck, e.trailer, e.gross, e.tare, e.net)
 	}
 }
 
-func newDoc(id *int) *entranceReceipt {
-	scanner := bufio.NewScanner(os.Stdin)
-	result := &entranceReceipt{
-		id: *id,
-	}
-	result.date = time.Now()
-	fmt.Println("Въведете номер на камион.")
-	scanner.Scan()
-	result.truck = scanner.Text()
-	fmt.Println("Въведете номер на ремарке.")
-	scanner.Scan()
-	result.trailer = scanner.Text()
-	fmt.Println("Въведете количество бруто.")
-	for {
-		scanner.Scan()
-		num, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			fmt.Printf("Невалидно число: %v", err)
-			continue
-		}
-		result.gross = num
-		break
-	}
-	for {
-		scanner.Scan()
-		num, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			fmt.Printf("Невалидно число: %v", err)
-			continue
-		}
-		result.tare = num
-		break
-	}
-	result.net = result.gross - result.tare
-	*id++
-	return result
+func newDoc(id *int, receipts *[]entranceReceipt) {
+	*receipts = append(*receipts, NewEntranceReceipt(id))
 }
